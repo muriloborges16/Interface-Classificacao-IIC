e75b52498000fa52a91f1c7ff39ce19f01903290
muriloborges16 <murilocb16@gmail.com>
2023-02-17 13:39:23 -0300

    Mudando o host para "127.0.0.1" modified:   main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def form():
     return render_template('resultado.html',titulo="Classificação do Cliente", resultado=resultado)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host='127.0.0.1', port=8080)
